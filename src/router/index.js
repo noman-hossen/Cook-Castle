d3617167b69aa01e118d6d0cc0d49d0c6b03d86d
noman-hossen <nomansfamily16@gmail.com>
2024-09-20 09:19:15 +0600

    Updated to use vite.config.js for GitHub Pages deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../views/Home.vue';
 import About from '../views/About.vue';
 import Contact from '../views/Contact.vue';
@@ -18,8 +18,8 @@ const routes = [
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
-  routes,
-});
+  history: createWebHashHistory(),
+  routes
+})
 
 export default router;
